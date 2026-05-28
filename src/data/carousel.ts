637e12a5570b3ebdcc477bbb30b2b51245162104
Jeff James <jjames315@live.com>
2026-05-28 14:07:49 -0700

    wording tweak

--- src/data/carousel.ts
+++ src/data/carousel.ts
@@ -9,7 +9,7 @@ export interface CarouselSlide {
 export const carouselSlides: CarouselSlide[] = [
   { tag: 'family' as Tag, image: '/images/carousel/Family.jpg', tagline: 'Role of partner, parent, sibling, son' },
   { tag: 'friends' as Tag, image: '/images/carousel/Friends.JPEG', tagline: 'Show-up, hear and celebrate friends' },
-  { tag: 'career' as Tag, image: '/images/carousel/Career.JPEG', tagline: 'Productivity from graduate school to big-tech' },
+  { tag: 'career' as Tag, image: '/images/carousel/Career.JPEG', tagline: 'Career from graduate school through big-tech' },
   { tag: 'alomac' as Tag, image: '/images/carousel/Alomac.JPEG', tagline: 'Entreprenural side-gig with my brother' },
-  { tag: 'legos' as Tag, image: '/images/carousel/Legos.JPEG', tagline: 'My own creations (MOC) in LEGO' },
+  { tag: 'legos' as Tag, image: '/images/carousel/Legos.JPEG', tagline: 'My serious LEGO hobby' },
 ];
